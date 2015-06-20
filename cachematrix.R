017dd3e5f6e3c6481317db6e4da0c8a5155a3344
Julia Piccardo <j_piccardo@yahoo.es>
2015-06-20 16:25:10 +0200

    first update

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The function createMatrix creates a new square matrix 'x
+ ## First we create a matrix x. We can do using this instructions:
+numberRowsColums <- as.integer(runif(1, min = 2, max = 10))
+numberCases <- numberRowsColums^2
+x <- matrix(runif(numberCases, min = 0, max = 99),numberRowsColums)
 
-makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+makeCacheMatrix <- function(x = matrix()) {
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inverse <<- solve
+        getinverse <- function() inverse
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+        
+}
 
-## Write a short comment describing this function
+## The function cacheSolve return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse        
+
 }
